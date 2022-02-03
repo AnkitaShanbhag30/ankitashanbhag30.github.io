6bd44ffbcc7f78923df56f06adde9ba604ce9f0d
Ankita Shanbhag <ankita.c339@gmail.com>
2022-02-03 00:53:17 -0800

    Using CORS Anywhere to use https to connect to Mapbox API

--- realtime-tracker/mapanimation.js
+++ realtime-tracker/mapanimation.js
@@ -1,4 +1,4 @@
-const issApi = 'http://api.open-notify.org/iss-now.json';
+const issApi = 'https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-now.json';
 var mapbox_key = config.MAPBOX_KEY;
 mapboxgl.accessToken = mapbox_key;
 var location;
